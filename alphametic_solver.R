fdf1828a1a424e09248ef405d8c9f2d586e5c8aa
barleytea <barleytea362@gmail.com>
2018-09-18 19:50:43 +0900

    made sure that can solve 'SEND+MORE=MONEY' (4:12.44 elapsed)

--- alphametic_solver.R
+++ alphametic_solver.R
@@ -1,49 +1,24 @@
-# Output the matrix of permutation nPr
-permutation <- function (n, r, v = 1:n, set = TRUE, repeats.allowed = FALSE) {
-  if (mode(n) != "numeric" || length(n) != 1 || n < 1 || (n%%1) != 0) {
-    stop("bad value of n")
-  }
+recite.permutations <- function (r) {
+  n <- 10
+  v <- 0:9
+
   if (mode(r) != "numeric" || length(r) != 1 || r < 1 || (r%%1) != 0) {
     stop("bad value of r")
   }
-  if (!is.atomic(v) || length(v) < n) {
-    stop("v is either non-atomic or too short")
+  if ((r > n)) {
+    stop("r > n")
   }
-  if ((r > n) & repeats.allowed == FALSE) {
-    stop("r > n and repeats.allowed=FALSE")
-  }
-  if (set) {
-    v <- unique(sort(v))
-    if (length(v) < n) { 
-      stop("too few different elements")
-    }
-  }
+
   v0 <- vector(mode(v), 0)
-  if (repeats.allowed) 
-    sub <- function(n, r, v) {
-      if (r == 1) { 
-        matrix(v, n, 1)
-      } else if (n == 1) { 
-        matrix(v, 1, r)
-      } else {
-        inner <- Recall(n, r - 1, v)
-        cbind(rep(v, rep(nrow(inner), n)), matrix(t(inner), 
-        ncol = ncol(inner), nrow = nrow(inner) * n, 
-        byrow = TRUE))
-      }
+  sub <- function(n, r, v) {
+    if (r == 1) {
+      matrix(v, n, 1)
     } else {
-      sub <- function(n, r, v) {
-      if (r == 1) { 
-        matrix(v, n, 1)
-      } else if (n == 1) {
-        matrix(v, 1, r)
-      } else {
-        X <- NULL
-        for (i in 1:n) {
-          X <- rbind(X, cbind(v[i], Recall(n - 1, r - 1, v[-i])))
-        }
-        X
+      X <- NULL
+      for (i in 1:n) {
+        X <- rbind(X, cbind(v[i], Recall(n - 1, r - 1, v[-i])))
       }
+      X
     }
   }
   sub(n, r, v[1:n])
@@ -58,21 +33,38 @@ AlphameticSolver <- function() {
 
   OPERANDS <- c("+", "-", "*", "(", ")", "=")
 
-  left.indexes <- which(!(former %in% OPERANDS))
-  right.indexes <- which(!(latter %in% OPERANDS))
+  left.indexes <- which(!(left %in% OPERANDS))
+  right.indexes <- which(!(right %in% OPERANDS))
 
   left.items <- unlist((strsplit(paste(left, collapse=""), "\\+")))
   right.items <- unlist((strsplit(paste(left, collapse=""), "\\+")))
 
-  number.candidates <- 1:length(unique(splitted[!(splitted %in% OPERANDS)]))
+  letters.candidates <- unique(splitted[!(splitted %in% OPERANDS)])
 
-  for (i in 1:length(left.indexes)) {
-    left[left.indexes[i]] <- former.solutions[i]
+  solutions <- rep(0, length(letters.candidates))
+  names(solutions) <- letters.candidates
+ 
+  res = c()
+  combination.candidates <- recite.permutations(length(letters.candidates))
+  combination.candidates <- combination.candidates[combination.candidates[,1] != 0, ]
+  for (i in 1:dim(combination.candidates)[1]) {
+	left.comp <- left; right.comp <- right;
+    solutions[1:length(solutions)] <- combination.candidates[i, ]
+    for (j in 1:length(left.indexes)) {
+      left.comp[left.indexes[j]] <- solutions[names(solutions) == left[left.indexes[j]]]
+	}
+    for (k in 1:length(right.indexes)) {
+      right.comp[right.indexes[k]] <- solutions[names(solutions) == right[right.indexes[k]]]
+	}
+	if (eval(parse(text=paste(left.comp, collapse=""))) == eval(parse(text=paste(right.comp, collapse="")))
+        & right.comp[1] != 0) {
+      res = c(res, paste(paste(left.comp, collapse=""), paste(right.comp, collapse=""), sep="="))
+    } 
   }
 
-  eval(parse(text=left))
-
-  write(lines, stdout())
+  for (i in 1:length(res)) {
+    write(res[i], stdout())
+  }
 }
 # driver code
 AlphameticSolver()
