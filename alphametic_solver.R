bf49286343deeb478109ee5655728bddd94eec43
barleytea <barleytea362@gmail.com>
2018-09-21 01:17:25 +0900

    - make sure can solve AB2DEF*2=2DEFAB

--- alphametic_solver.R
+++ alphametic_solver.R
@@ -26,20 +26,29 @@ recite.permutations <- function (r) {
 
 
 AlphameticSolver <- function() {
+  # constants
+  OPERATOR <- c("+", "-", "*")
+  EQUAL_SIGN <- "="
+  PARENTHESES <- c("(", ")")
+  NUMBERS <- c("1", "2", "3", "4", "5", "6", "7", "8", "9")
+  NO_ALP <- c(OPERATOR, EQUAL_SIGN, PARENTHESES, NUMBERS)
+
   lines <- readLines("stdin")
+
+  # split character by character
   splitted <- noquote(strsplit(lines, split=NULL)[[1]])
+  # split into the left and the right
   left <- noquote(strsplit(substr(lines, 1, which(splitted == "=") - 1), split=NULL)[[1]])
   right <- noquote(strsplit(substr(lines, which(splitted == "=") + 1, length(splitted)), split=NULL)[[1]])
 
-  OPERANDS <- c("+", "-", "*", "(", ")", "=")
+  # indexes of alphabets
+  left.alp.indexes <- which(!(left %in% NO_ALP))
+  right.alp.indexes <- which(!(right %in% NO_ALP))
 
-  left.indexes <- which(!(left %in% OPERANDS))
-  right.indexes <- which(!(right %in% OPERANDS))
-
-  left.items <- unlist((strsplit(paste(left, collapse=""), "(\\+|\\-|\*)")))
+  left.items <- unlist((strsplit(paste(left, collapse=""), "(\\+|\\-|\\*)")))
   right.items <- unlist((strsplit(paste(right, collapse=""), "(\\+|\\-|\\*)")))
 
-  letters.candidates <- unique(splitted[!(splitted %in% OPERANDS)])
+  letters.candidates <- unique(splitted[!(splitted %in% NO_ALP)])
 
   solutions <- rep(0, length(letters.candidates))
   names(solutions) <- letters.candidates
@@ -50,15 +59,15 @@ AlphameticSolver <- function() {
   for (i in 1:dim(combination.candidates)[1]) {
     left.comp <- left; right.comp <- right;
     solutions[1:length(solutions)] <- combination.candidates[i, ]
-    for (j in 1:length(left.indexes)) {
-      left.comp[left.indexes[j]] <- solutions[names(solutions) == left[left.indexes[j]]]
+    for (j in 1:length(left.alp.indexes)) {
+      left.comp[left.alp.indexes[j]] <- solutions[names(solutions) == left[left.alp.indexes[j]]]
     }
-    for (k in 1:length(right.indexes)) {
-      right.comp[right.indexes[k]] <- solutions[names(solutions) == right[right.indexes[k]]]
+    for (k in 1:length(right.alp.indexes)) {
+      right.comp[right.alp.indexes[k]] <- solutions[names(solutions) == right[right.alp.indexes[k]]]
     }
     left.pasted <- paste(left.comp, collapse="")
     right.pasted <- paste(right.comp, collapse="")
-    if (eval(parse(text=left.pasted) == eval(parse(text=right.pasted) & right.comp[1] != 0) { # XXX
+    if (eval(parse(text=left.pasted)) == eval(parse(text=right.pasted)) & right.comp[right.alp.indexes[1]] != 0) { # XXX
       res <- c(res, paste(left.pasted, right.pasted, sep="="))
     }
   }
